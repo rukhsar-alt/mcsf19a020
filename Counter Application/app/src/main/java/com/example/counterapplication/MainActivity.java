71551a5e980caf3691a8ff29a30ef67f0f08bbb3
mcsf19a020 <mcsf19a020@pucit.edu.pk>
2021-05-31 17:34:38 +0500

    Counter Application Completed

--- Counter Application/app/src/main/java/com/example/counterapplication/MainActivity.java
+++ Counter Application/app/src/main/java/com/example/counterapplication/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.counterapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
+    Button button;
+    int count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void countIt(View view) {
+        count=Integer.parseInt(textView.getText().toString());
+        count++;
+        textView.setText(String.valueOf(count));
+
     }
 }
