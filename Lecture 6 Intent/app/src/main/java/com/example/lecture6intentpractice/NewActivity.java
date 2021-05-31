2011a4bf4bb3007ae8e48c519b5076af805de994
mcsf19a020 <mcsf19a020@pucit.edu.pk>
2021-05-31 17:15:45 +0500

    Lecture 6 Completed

--- Lecture 6 Intent/app/src/main/java/com/example/lecture6intentpractice/NewActivity.java
+++ Lecture 6 Intent/app/src/main/java/com/example/lecture6intentpractice/NewActivity.java
@@ -2,7 +2,10 @@ package com.example.lecture6intentpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class NewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
+    }
+    public void Call(View view)
+    {
+        Uri uri= Uri.parse("tel: +923209407871");
+        Intent intent=new Intent(Intent.ACTION_DIAL,uri);
+        startActivity(intent);
+    }
+    public void openWebsite(View view)
+    {
+        Uri uri= Uri.parse("https://www.google.com");
+        Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
     }
 }
