372f8a18c6dc951eca781c69b9c08732b6aa819e
mcsf19a020 <mcsf19a020@pucit.edu.pk>
2021-05-31 16:50:23 +0500

    Lecture 3 Completed

--- Lecture 3/app/src/main/java/com/example/lecture3practice/screen2.java
+++ Lecture 3/app/src/main/java/com/example/lecture3practice/screen2.java
@@ -2,7 +2,9 @@ package com.example.lecture3practice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class screen2 extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class screen2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen2);
+    }
+    public void mainScreen(View view) {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
