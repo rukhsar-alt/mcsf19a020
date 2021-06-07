b642c775391a0a089b945789713087cec052f01d
mcsf19a020 <mcsf19a020@pucit.edu.pk>
2021-06-07 18:28:13 +0500

    User Class created

---
+++ Recycler View Application/app/src/main/java/com/example/recyclerviewproject/User.java
@@ -0,0 +1,50 @@
+package com.example.recyclerviewproject;
+
+public class User {
+    private int id;
+    private String name;
+    private String age;
+    private String city;
+
+    public User(int id, String name, String age, String city) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
+
+}
